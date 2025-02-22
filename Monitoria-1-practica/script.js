e2a581fd114f65e9431c156b2a0c9abfa9e3c602
SantiagoV0106 <velascosantiago3@gmail.com>
2025-02-22 17:51:30 -0500

    Add render episodios por personajes

--- Monitoria-1-practica/script.js
+++ Monitoria-1-practica/script.js
@@ -20,6 +20,7 @@ async function getCharacters() {
         }
 
     } catch (error) {
+        console.error(error);
 
     }
 
@@ -32,29 +33,64 @@ getCharacters()
 const renderPersonajes = (personajes) => {
     const body = document.querySelector('body')
 
-
     personajes.forEach(personaje => {
 
-        const episodes = personaje.episode
-        console.log(episodes);
-
-        episodes.forEach(async (episode) => {
-            const res = await fetch(episode)
-            const data = await res.json()
-            console.log(personaje.name, data);
-
-        });
-    });
-
-    personajes.forEach(personaje => {
         const card = document.createElement('div');
         card.innerHTML = `
         <p> ${personaje.name}</p>
         <img src='${personaje.image}'/>
         `
 
+        const episodes = personaje.episode
+
+        episodes.slice(0, 5).forEach(async (episode) => {
+            const res = await fetch(episode)
+            const data = await res.json()
+
+
+            const episodesContainer = document.createElement('div')
+            episodesContainer.innerHTML = `
+            <p> Episode: ${data.name} </p>
+            `
+            card.appendChild(episodesContainer);
+
+        });
+
         body.appendChild(card)
 
     });
 
+    personajes.forEach(personaje => {
+
+
+    });
+
 }
+
+
+// Opción para traer los episodios, por personaje, con el método for. Recuerden, como es por personaje esta función debería ir dentro de la función que pinta cada personaje renderPersonajes()
+
+const getEpisodes = async () => {
+
+    for (let i = 0; i < episodes.length; i++) {
+        // Le decimos que cuando i sea igual a 5, deje de sumar. Es decir, que si el arrgelo tiene Xs cantidad de datos, solo llega hasta el quinto
+        if (i === 5) break;
+
+        const element = episodes[i];
+
+        const res = await fetch(element)
+
+        const data = await res.json()
+
+        console.log(data);
+
+        const episodesContainer = document.createElement('div')
+
+        episodesContainer.innerHTML = `
+        <p> Episode: ${data.name} </p>
+        `
+        card.appendChild(episodesContainer);
+    }
+}
+
+// getEpisodes()
